3367686df1edc230123f46387998da3cb8824972
TimLit13 <litidm@gmail.com>
2021-06-10 14:23:25 +0300

    Refactor edit structure of app

--- app.rb
+++ app.rb
@@ -1,31 +1,31 @@
 #encoding: cp866
 require 'mechanize'
-require_relative 'open_file.rb'
-require_relative 'extract_hash_from_file.rb'
+require_relative 'helpers/open_file.rb'
+require_relative 'helpers/extract_hash_from_file.rb'
 
 #загрузить список городов
-file_list_of_cities = open_file ('list_of_cities.txt')
+file_list_of_cities = open_file ('source/list_of_cities.txt')
 
 #создать хэш из файла с городами и их идентификаторами
 @cities_hash = extract_hash_from_file (file_list_of_cities)
 
+# -----------------------------
+# def debug
+#   @cities_hash.each do |key, value|
+#     puts '='*80
+#     puts "#{key}\t#{value}"
+#   end
+#   puts '='*80
+# end
+# -----------------------------
 
-
-#-----------------------------
-# debug
-
-@cities_hash.each do |key, value|
-  puts '='*80
-  puts "#{key}\t#{value}"
-end
-puts '='*80
-
-#-----------------------------
-
+puts "Какой город нужен"
 
 
 #Создать экземпляр "браузера""
 agent = Mechanize.new()
+
+
 
 #скачать страницу
 # page = agent.get("https://www.meteoservice.ru/weather/14days/sankt-peterburg")
