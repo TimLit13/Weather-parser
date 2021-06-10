9929e1db27d748dd312231d33cb1c42046d92c16
TimLit13 <83809986+TimLit13@users.noreply.github.com>
2021-06-10 14:39:13 +0300

    Add translit gem

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 #encoding: cp866
 require 'mechanize'
+require 'translit'
 require_relative 'helpers/open_file.rb'
 require_relative 'helpers/extract_hash_from_file.rb'
 
@@ -10,16 +11,24 @@ file_list_of_cities = open_file ('source/list_of_cities.txt')
 @cities_hash = extract_hash_from_file (file_list_of_cities)
 
 # -----------------------------
-# def debug
-#   @cities_hash.each do |key, value|
-#     puts '='*80
-#     puts "#{key}\t#{value}"
-#   end
-#   puts '='*80
-# end
+
+def debug
+  @cities_hash.each do |key, value|
+    puts '='*80
+    puts "#{key}\t#{value}"
+  end
+  puts '='*80
+end
+
+debug()
+
 # -----------------------------
 
-puts "Какой город нужен"
+puts "Прогноз погоды"
+print "Укажите, какой город нужен: "
+user_city = gets.chomp.downcase
+user_city = Translit.convert(user_city, :english)
+puts user_city
 
 
 #Создать экземпляр "браузера""
