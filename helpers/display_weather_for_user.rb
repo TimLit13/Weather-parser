c28c774ba2764498b3883d55159d46dbef369b38
TimLit13 <83809986+TimLit13@users.noreply.github.com>
2021-06-11 08:07:13 +0300

    Add display weather helper

---
+++ helpers/display_weather_for_user.rb
@@ -0,0 +1,13 @@
+def show_weather
+end
+
+
+
+# puts "Дата:"
+# puts "Погода:"
+# puts "Температура макс:"
+# puts "Температура мин:"
+# puts "Вероятность осадков:"
+# puts "Ветер:"
+
+# puts "Прогноз погоды взят с сайта: \"https://www.meteoservice.ru/\""
