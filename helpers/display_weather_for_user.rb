a3697f272dafde2be70178da869494440c86dc14
TimLit13 <83809986+TimLit13@users.noreply.github.com>
2021-06-11 09:16:40 +0300

    Add handler for display information

--- helpers/display_weather_for_user.rb
+++ helpers/display_weather_for_user.rb
@@ -1,13 +1,28 @@
-def show_weather
+def show_weather (options={})
+  all_days_of_week = options[:all_days_of_week]
+  all_dates = options[:all_dates]
+  all_weather = options[:all_weather]
+  all_temperature = options[:all_temperature]
+  chance_of_precipitation = options[:chance_of_precipitation]
+  all_winds = options[:all_winds]
+
+  (0..13).each do |index|
+    puts '='*80
+    print "Дата: "
+    puts all_days_of_week[index]
+    print "Погода: "
+    puts all_dates[index]
+    print "Температура макс: "
+    puts all_weather[index]
+    print "Температура мин: "
+    puts all_temperature[index]
+    print "Вероятность осадков: "
+    puts chance_of_precipitation[index]
+    print "Ветер: "
+    puts all_winds[index]
+      
+  end
+  puts '='*80
+  puts "Прогноз погоды взят с сайта: \"https://www.meteoservice.ru/\""
 end
 
-
-
-# puts "Дата:"
-# puts "Погода:"
-# puts "Температура макс:"
-# puts "Температура мин:"
-# puts "Вероятность осадков:"
-# puts "Ветер:"
-
-# puts "Прогноз погоды взят с сайта: \"https://www.meteoservice.ru/\""
