f0f1e652e4aae4f705cec7f88b23154be65f4382
TimLit13 <83809986+TimLit13@users.noreply.github.com>
2021-06-11 09:16:16 +0300

    Fix encoding from extract file

--- helpers/extract_hash_from_file.rb
+++ helpers/extract_hash_from_file.rb
@@ -4,13 +4,16 @@ def extract_hash_from_file (file_list_of_cities)
 
 file_list_of_cities.each_line do |line|
   temp = []
-  temp = line.strip.split(/[:,]/)
+  line = line.strip.gsub(/[^\p{L}\s\d]/,'').gsub(/[\u{10000}-\u{FFFFF}]/,'').delete('\\')
+  temp = line.split(" ")
 
+  # #debug
   # puts temp
   # puts temp.inspect
   # puts temp.length
  
   @cities[temp[1]] = temp[0]
 end
+
   return @cities
 end
